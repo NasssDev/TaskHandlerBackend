1d63c2b42b911165edf5bda94bcd8945b5583e98
Nasssdev <aissaoui_nassim@outlook.fr>
2024-12-09 02:24:26 +0100

    fix cors

--- config/constants.js
+++ config/constants.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 
-const FRONTEND_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://freegaza.vercel.app'
-  : 'http://localhost:5173';
+const FRONTEND_URL = 'https://freegaza.vercel.app'
 
 module.exports = {
   FRONTEND_URL,
