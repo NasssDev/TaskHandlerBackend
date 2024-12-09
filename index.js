8ab1c580bd8674a571cd429b0d638a889417e075
Nasssdev <aissaoui_nassim@outlook.fr>
2024-12-09 09:06:23 +0100

    fix cors for frontend static url

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: FRONTEND_URL,
+  origin: ['https://freegaza.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
@@ -21,7 +21,7 @@ app.use(cors({
 }));
 
 app.options('*', cors({
-  origin: FRONTEND_URL,
+  origin: ['https://freegaza.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
